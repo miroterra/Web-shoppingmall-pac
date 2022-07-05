d4fc6b49a5a6ca58c58a0298be72f0c7a78022ec
Takphantom <miroterra@naver.com>
2022-07-05 20:21:08 +0900

    2.5

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path'); // 노드JS 내장된 경로 패키지
 
 const express = require('express'); //익스프레스 가져오기
 
+const db = require('./data/database'); // 데이터서버 연동
 const authRoutes = require('./routes/auth-routes'); // auth 라우트 가져오기
 
 const app = express();
@@ -13,4 +14,11 @@ app.use(express.static('public')); // 정적으로 제공
 
 app.use(authRoutes);
 
-app.listen(3000);
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log('Failed to connect to the database');
+    console.log(error);
+  });
